55213812e013880f41bbafac2fcd56bc66f85a01
Fernanda <20220031@ricaldone.edu.sv>
2024-09-24 16:12:38 -0600

    base de datos

---
+++ tbescritor.sql
@@ -0,0 +1,8 @@
+create table tbescritor (
+uuidescritor varchar2 (100),
+nombre_escritor varchar2(100),
+edad_escritor varchar2(100),
+peso_escritor varchar2 (100),
+correo_escritor varchar2(100)
+
+);
